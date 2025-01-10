cd8b64f45df541f79c04487bc8b6e8daf7b1c44e
Prince Vaviya <1princevaviya@gmail.com>
2025-01-10 12:53:05 +0530

    AscendDescend

---
+++ Documents/JAVA/JAVA Questions/printReverse.java
@@ -0,0 +1,30 @@
+//Print the numbers in ascending and descending order 
+
+import java.util.Scanner;
+
+class printReverse{
+    public static void main(String args[]){
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.print("Enter the beginner number : ");
+        int beg = myObj.nextInt();
+        System.out.print("Enter the ending number : ");
+        int end = myObj.nextInt();
+
+        System.out.println("Enter the option : ");
+        System.out.println("1. Ascending order ");
+        System.out.println("2. Descending order ");
+        int opt = myObj.nextInt();
+
+        if(opt==1){ 
+            for(int i = beg; i<=end; i++){
+                System.out.println(i);
+            }
+        }
+        else if(opt==2){ 
+            for(int i = beg; i>=end; i--){
+                System.out.println(i);
+            }
+        }
+    }
+}
